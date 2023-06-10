887e916853618d1e5230161c9db3665644b72ac5
邱依良 <553579048@qq.com>
2023-06-11 00:20:58 +0800

    [update] LCS

--- src/main/java/algorithms/dp/Offer095.java
+++ src/main/java/algorithms/dp/Offer095.java
@@ -6,17 +6,38 @@ package algorithms.dp;
  * @author: qyl
  */
 public class Offer095 {
+    String text1, text2;
+    int[][] dp;
+    String commonString  = "";
+
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length () + 1][text2.length () + 1];
-        for (int i = 0; i < text1.length(); i++) {
-            for (int j = 0; j < text2.length(); j++) {
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i+1][j+1] = dp[i][j] + 1;
-                }else{
-                    dp[i+1][j+1] = Math.max (dp[i][j+1],dp[i+1][j]);
+        this.text1 = text1;
+        this.text2 = text2;
+        int n = text1.length ();
+        int m = text2.length ();
+        dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (text1.charAt (i) == text2.charAt (j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max (dp[i][j + 1], dp[i + 1][j]);
                 }
             }
         }
-        return dp[text1.length ()][text2.length()];
+        int i = n - 1, j = m - 1;
+        while(i >= 0 && j >= 0){
+            if (text1.charAt (i) == text2.charAt(j)){
+                commonString = text1.charAt(i) + commonString;
+                i--;j--;
+            }else if (dp[i][j + 1] > dp[i+1][j]){
+                i--;
+            }else {
+                j--;
+            }
+        }
+        System.out.println (commonString);
+        return dp[n][m];
     }
+
 }
